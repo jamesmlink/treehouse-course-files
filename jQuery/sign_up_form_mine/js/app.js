6e31b8dca994dcde1a79685f8f17de31be0f443e
James Link <jamesmlink@gmail.com>
2016-09-07 18:33:29 -0400

    jQ sign-up form project start

---
+++ jQuery/sign_up_form_mine/js/app.js
@@ -0,0 +1,16 @@
+//Problem: Hints are shown even when form is valid
+//Solution: Hide and show them at appropriate times
+
+//hide Hints
+
+//when event happens on pw input
+//find out if pw is valid
+  //hide hint if valid
+  //else show it
+
+//when event happens on confirmation input
+  //find out if match
+  //hide hint if matched
+  //else show hint
+
+  
