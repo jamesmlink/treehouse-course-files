0bb4d8f96de56c2f3db5ded394d819b0f0284f24
James Link <jamesmlink@gmail.com>
2016-09-08 19:44:04 -0400

    finish signup form jQ project

--- jQuery/sign_up_form_mine/js/app.js
+++ jQuery/sign_up_form_mine/js/app.js
@@ -1,16 +1,36 @@
 //Problem: Hints are shown even when form is valid
 //Solution: Hide and show them at appropriate times
 
+
+var $password = $("#password");
+var $confirmPassword = $("#confirm_password");
+
 //hide Hints
+$("form span").hide();
+
+function passwordEvent (){
+  //find out if pw is valid
+  if($password.val("length") > 8){
+    //hide hint if valid
+    $password.next().hide();
+  } else {
+    //else show it
+    $password.next().show();
+  }
+};
+
+function confirmPasswordEvent (){
+  //find out if match
+  if($password.val() === $confirmPassword.val()) {
+  //hide hint if matched
+    $confirmPassword.next().hide();
+  } else {
+    $confirmPassword.next().show();
+  //else show hint
+  }
+};
 
 //when event happens on pw input
-//find out if pw is valid
-  //hide hint if valid
-  //else show it
-
+$password.focus(passwordEvent).keyup(passwordEvent).focus(confirmPasswordEvent).keyup(confirmPasswordEvent);
 //when event happens on confirmation input
-  //find out if match
-  //hide hint if matched
-  //else show hint
-
-  
+$confirmPassword.focus(confirmPasswordEvent).keyup(confirmPasswordEvent);
