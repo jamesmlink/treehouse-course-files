d7b3d91e84eb1016559decbd88f45ab76b30d0ed
James Link <jamesmlink@gmail.com>
2016-09-13 11:17:03 -0400

    jQuery plugins sticky nav challenge

--- jQuery/Using Plugins/section2/video02/js/main.js
+++ jQuery/Using Plugins/section2/video02/js/main.js
@@ -6,3 +6,24 @@ $(".animsition").animsition({
   outDuration: 500
 });
 
+$(".header").sticky();
+
+$(".header").on("sticky-start",function(){
+  $(".description").html("We build <strong>great</strong> apps");
+});
+
+$(".header").on("sticky-end",function(){
+  $(".description").html("We build apps");
+});
+
+$("h5").sticky({
+  topSpacing: 64
+});
+
+$("h5").on("sticky-start",function(){
+  $("h5").html("Want us to work on your project? <a>Email Us.</a>");
+});
+
+$("h5").on("sticky-end",function(){
+  $("h5").html("Want us to work on your project?");
+});
